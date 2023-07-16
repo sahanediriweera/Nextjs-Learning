153300b9b550a3ffe0fd49a37e2b0706ef5b0489
SCARRAIDER <rsahanediriweera@gmail.com>
2023-07-16 12:50:58 +0530

    starting nextjs

---
+++ next02/app/about/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function AboutLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+    <>
+        <nav>
+            About Navbar
+        </nav>
+        <main>
+            {children}
+        </main>
+    </>
+  )
+}

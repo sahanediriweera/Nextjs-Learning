025475e1c3569972d87e5004fe73847c26d3a118
SCARRAIDER <rsahanediriweera@gmail.com>
2023-07-17 21:00:58 +0530

    completed the middle ware

---
+++ next10/app/page.tsx
@@ -0,0 +1,19 @@
+import Posts from "./components/Posts";
+import MyProfilePic from './components/MyProfilePic'
+
+export const revalidate = 10
+
+export default function Home() {
+  return (
+    <div className="mx-auto">
+      <MyProfilePic/>
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          I&apos;m <span className="font-bold">Sahan Ediriweera</span>.
+        </span>
+      </p>
+      <Posts />
+    </div>
+  )
+}
